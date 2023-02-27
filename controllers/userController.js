aa60703a8d87e9177ae6bdb453d2b66c04b50d4e
tahminaroshni <tahminatanni12@gmail.com>
2023-02-27 16:25:45 +0600

    user successfully created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,22 @@
-const mongoose = require('mongoose');
+const User = require('../models/userModel');
 
 const loginUser = async (req, res) => {
   res.json({ message: 'login' })
 }
 
 const signupUser = async (req, res) => {
-  res.json({ message: 'signup' })
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json(user);
+
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 }
 
 module.exports = {
   loginUser,
   signupUser
 }
+
